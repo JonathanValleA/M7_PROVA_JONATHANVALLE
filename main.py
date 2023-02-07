654d496997d16de1501e35e10bf202e11abebeef
JonathanValleA <jvalle@jaumebalmes.net>
2023-02-07 20:37:28 +0100

    Examen Ejercicio 1 y 2

---
+++ main.py
@@ -0,0 +1,7 @@
+import ejercicio1 as eje1
+import ejercicio2 as eje2
+
+print("----- Ejercicio 1 Mitjana NOTA ALUMNAT CICLE -----")
+eje1.ejercicio1()
+print("----- Ejercicio 2 % de faltes DAW2 -----")
+eje2.ejercicio2()
